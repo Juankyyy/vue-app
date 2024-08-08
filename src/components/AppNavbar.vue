b2f2b998eca315fb704137cb7bd4942f56d0340c
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-08-08 11:39:04 -0500

    feat: Bootstrap, AppNavbar, BusinessNavbar, etc
    Se agrega Bootstrap, se crean navbar de la app y navbar de la plantilla, scss global para normalizar Bootstrap

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app-navbar">
+        <router-link to="/">Inicio</router-link>
+        <router-link to="/about">Acerca de</router-link>
+        <router-link to="/services">Servicios</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppNavbar"
+    }
+</script>
+
+<style lang="scss">
+    .app-navbar {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        margin: 10px;
+        background-color: #222;
+
+        border-radius: 10px;
+
+        a {
+            font-size: 20px;
+            text-decoration: none;
+            color: #fff;
+            margin: 0px 10px;
+            transition: all 0.2s;
+
+            &:hover {
+                color: #bebebe;
+            }
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router"
 
+
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import "./assets/app.scss";
+
 createApp(App).use(router).mount('#app')
